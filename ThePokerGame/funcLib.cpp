1bea5270e301c1f44ebd3c93c40a7427f13e809d
chouguting <chouguting@gmail.com>
2020-04-12 17:25:53 +0800

    加上註解,簡化程式

--- ThePokerGame/funcLib.cpp
+++ ThePokerGame/funcLib.cpp
@@ -4,8 +4,10 @@
 #include "funclib.h"
 
 
+//儲存各種牌型的分數
 int cardTypeScore[] = {0, 60, 120, 200, 280, 360, 500, 800, 1000};
 
+//以牌面數字為優先的填牌方式(梅花2,菱形2,紅心2,黑桃2,梅花3......)
 void fillDeckFaceFirst(Card* const wDeck, const char* wFace[],
                        const char* wSuit[])
 {
@@ -16,26 +18,16 @@ void fillDeckFaceFirst(Card* const wDeck, const char* wFace[],
 		int k;
 		for (k = 0; k < 4; k++)
 		{
-			wDeck[i].face = wFace[j];
-			wDeck[i].suit = wSuit[k];
-			wDeck[i].id = i;
+			wDeck[i].face = wFace[j]; //寫入牌面數字
+			wDeck[i].suit = wSuit[k]; //寫入牌面花色
+			wDeck[i].id = i; //給每一張牌id,越大的牌id越大(0至52)
 			i++;
 		}
 	}
 }
 
-void fillDeck(Card* const wDeck, const char* wFace[],
-              const char* wSuit[])
-{
-	int i;
 
-	for (i = 0; i <= 51; i++)
-	{
-		wDeck[i].face = wFace[i % 13];
-		wDeck[i].suit = wSuit[i / 13];
-	}
-}
-
+//隨機洗牌(洗52張wDeck的牌)
 void shuffle(Card* const wDeck)
 {
 	int i, j;
@@ -50,16 +42,19 @@ void shuffle(Card* const wDeck)
 	}
 }
 
+
+//發牌(發wDeck的牌，共發quantity張)
 void deal(const Card* const wDeck, int quantity)
 {
 	int i;
 
 	for (i = 0; i < quantity; i++)
+		//印出第幾張,甚麼牌,和他的id
 		printf("\t第%d張牌 \t %5s of %-8s \t%d%c", i + 1, wDeck[i].face,
 		       wDeck[i].suit, wDeck[i].id, '\n');
 }
 
-
+//依次印出所有的牌，並且分成playerCount,每份13張
 void dealManyPlayer(int playerCount, const Card* const wDeck)
 {
 	int i;
@@ -80,14 +75,15 @@ void dealManyPlayer(int playerCount, const Card* const wDeck)
 }
 
 
-//判斷同花
+//判斷同花(回傳1是，0不是)
 int isFlush(Card wDeck[], int size)
 {
-	int isflush = 1;
+	int isflush = 1; //先假設他是同花
 	int i;
-	int lastSuit = (wDeck[0].id + 1) % 4;
+	int lastSuit = (wDeck[0].id + 1) % 4; //上一張牌的花色
 	for (i = 0; i < size; i++)
 	{
+		//如果檢查到一張花色不一樣的,就沒有同花了
 		if ((wDeck[i].id + 1) % 4 != lastSuit)
 		{
 			isflush = 0;
@@ -98,46 +94,50 @@ int isFlush(Card wDeck[], int size)
 	return isflush;
 }
 
-//判斷順
+//判斷順(回傳1是，0不是)
 int isStraight(Card wDeck[], int size)
 {
-	int isstraight = 1;
+	int isstraight = 1; //先假設他是順子
 	int i;
 
-	int lastFace = (wDeck[0].id) / 4;
+	int lastFace = (wDeck[0].id) / 4; //上一張牌的牌面數字
 	for (i = 1; i < size; i++)
 	{
-		//printf("FACE: %d LAST:%d\n", (wDeck[i].id) / 4,lastFace);
+		//如果檢查到一張牌跟上一張不連續就不是順了
 		if ((wDeck[i].id) / 4 != lastFace + 1)
 		{
 			isstraight = 0;
 		}
-		lastFace = (wDeck[i].id) / 4;
+		lastFace = (wDeck[i].id) / 4; //每檢查完一張這張牌就會變成下張的上一張牌
 	}
 	return isstraight;
 }
 
 
-//判斷A2345的順 (特殊的)
+//判斷特殊的(A2345)的順 (回傳1是，0不是)
 int isSpecialStraight(Card wDeck[], int size)
 {
-	int isspecialstraightraight = 1;
+	int isspecialstraightraight = 1; //先預設他是特殊順
 	int i;
 
-	if (wDeck[0].id / 4 != 0)
+	if (wDeck[0].id / 4 != 0) //如果最id小的那張的數字不是2,就不成立
 	{
 		isspecialstraightraight = 0;
 	}
 
+	//和上面順一樣的檢查方式(這次檢查到倒數第二張)
 	int lastFace = (wDeck[0].id) / 4;
 	for (i = 1; i < size - 1; i++)
 	{
+		//如果檢查到一張牌跟上一張不連續就不是順了
 		if ((wDeck[i].id) / 4 != lastFace + 1)
 		{
 			isspecialstraightraight = 0;
 		}
-		lastFace = (wDeck[i].id) / 4;
+		lastFace = (wDeck[i].id) / 4; //每檢查完一張這張牌就會變成下張的上一張牌
 	}
+
+	//id最大的那張牌數字應該要是A
 	if (wDeck[size - 1].id / 4 != 12)
 	{
 		isspecialstraightraight = 0;
@@ -146,16 +146,20 @@ int isSpecialStraight(Card wDeck[], int size)
 }
 
 
-//判斷給定值數量的牌出現幾次
+/*判斷給定值數量的牌出現幾次
+  要判斷對子的話第一個參數就輸入2,三條就3,四條就4
+  (回傳值是出現了幾次)
+*/
 int howManyOfaKind(int multipleKind, Card wDeck[], int size)
 {
-	int howMany = 0;
+	int howMany = 0; //出現了幾次
 	int i = 0;
+
+	//從第i個開始往後找，看是不是同數字的剛好有multipleKind張
 	while (i <= size - multipleKind)
 	{
-		int hasCorrectNumber = 1;
-		int currentFace = (wDeck[i].id) / 4;
-		int sameCount = 0;
+		int currentFace = (wDeck[i].id) / 4; //儲存這輪檢查牌的數字
+		int sameCount = 0; //往後找看有幾張牌一樣
 		int j = 0;
 
 		while (currentFace == (wDeck[i + j].id) / 4 && j + i < size)
@@ -163,26 +167,26 @@ int howManyOfaKind(int multipleKind, Card wDeck[], int size)
 			sameCount++;
 			j++;
 		}
+		//如果一樣數字的牌的數量剛好和multipleKind,才算有找到我們要的
 		if (sameCount == multipleKind)
 		{
-			i = i + multipleKind;
+			i = i + multipleKind; //檢查過的牌不用再檢查
 			howMany++;
 		}
 		else
 		{
-			i = i + sameCount;
+			i = i + sameCount; //檢查過的牌不用再檢查
 		}
 	}
 	return howMany;
 }
 
 
-//排序
+//排序(在wDeck中排序size張)
 void sort(Card wDeck[], int size)
 {
-	int i;
-
-	for (i = 0; i < size - 1; i++)
+	//找到最小的就往前排
+	for (int i = 0; i < size - 1; i++)
 	{
 		Card min = wDeck[i];
 		int minIndex = i;
@@ -202,6 +206,9 @@ void sort(Card wDeck[], int size)
 	}
 }
 
+//要先SORT!!
+//判斷wDeck中大小為size中的排是甚麼牌型
+//回傳牌型的index
 int determine(Card wDeck[], int size)
 {
 	sort(wDeck, size);
@@ -241,9 +248,14 @@ int determine(Card wDeck[], int size)
 }
 
 
+//要先SORT!!
+/*找到一組牌中,可比較的最大牌是哪一張
+  例如葫蘆中有一個三條加上一個對子，但要比較的是其中三條中的最大那張，而不是五張中最大的那張
+  回傳是最重要那張的id*/
 Card mostImportantCard(int multipleKind, Card wDeck[], int size)
 {
-	int i = size - 1;
+	//基本上和howManyOfaKind5找法一樣，但這次是從後面找回來
+	int i = size - 1; //從Size-1開始找
 	Card veryImoprtantCard = wDeck[i];
 	while (i >= multipleKind - 1)
 	{
@@ -261,45 +273,43 @@ Card mostImportantCard(int multipleKind, Card wDeck[], int size)
 			veryImoprtantCard = wDeck[i];
 			i = i - multipleKind;
 
-			return veryImoprtantCard;
+			return veryImoprtantCard; //只要一找到就直接return了
 		}
 		i = i - sameCount;
 	}
 	return veryImoprtantCard;
 }
 
+
+//要先SORT!!
+//給分，回傳是這副牌的分數(牌面分加牌型分)
 int giveScore(Card wDeck[], int size)
 {
-	int type = determine(wDeck, size);
+	int type = determine(wDeck, size); //判斷牌型
 	int score = 0;
-	if (isSpecialStraight(wDeck, size) == 1)
-	{
-		score = score + (wDeck[size - 2].id);
-	}
-	else if (type == 7)
+
+	//牌面分數
+	if (type == 7) //四條: 利用mostImportantCard找出四條中最大的那一張
 	{
 		score = score + mostImportantCard(4, wDeck, size).id;
 	}
-	else if (type == 6)
+	else if (type == 6) //葫蘆: 利用mostImportantCard找出葫蘆裡三條中最大的那一張
 	{
 		score = score + mostImportantCard(3, wDeck, size).id;
 	}
-	else if (type == 3)
+	else if (type == 3) //三條: 利用mostImportantCard找出三條中最大的那一張
 	{
 		score = score + mostImportantCard(3, wDeck, size).id;
 	}
-	else if (type == 2)
+	else if (type == 2 || type == 1) //兩對或對子:找到最大對子的最大那張
 	{
 		score = score + mostImportantCard(2, wDeck, size).id;
 	}
-	else if (type == 1)
-	{
-		score = score + mostImportantCard(2, wDeck, size).id;
-	}
-	else
+	else //其他牌型: 比較最大的那一張
 	{
 		score = score + (wDeck[size - 1].id);
 	}
-	score = score + cardTypeScore[type];
+
+	score = score + cardTypeScore[type]; //再加上牌型分數
 	return score;
 }
--- ThePokerGame/main.cpp
+++ ThePokerGame/main.cpp
@@ -4,12 +4,13 @@
 
 #include "funclib.h"
 
+void gameInit(); //遊戲初始化(選擇人數，制定換牌規則)
+Card* handCard[4]; //四副手牌
+int changeWithWhom[4]; //各個玩家要跟誰換牌
+int playerCount; //儲存有幾個玩家(支援2或4人)
+Card deck[52]; //牌堆
 
-void gameInit();
-
-Card* handCard[4];
-int changeWithWhom[4], playerCount;
-Card deck[52];
+//牌面(數字)
 const char* face[] = {
 	"Deuce", "Three",
 	"Four", "Five",
@@ -18,8 +19,10 @@ const char* face[] = {
 	"Jack", "Queen", "King", "Ace"
 };
 
+//牌面(花色)
 const char* suit[] = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
+//這個陣列是牌型(順序配合Determine)
 const char* cardType[] = {"散牌", "對子", "二對", "三條", "順子", "同花", "葫蘆", "四條", "同花順"};
 
 
@@ -34,13 +37,13 @@ int main()
 	printf("歡迎來到神奇的撲克遊戲!!\n");
 
 
-	gameInit();
+	gameInit(); //初始化
 
 
-	int operation = 0;
-	fillDeckFaceFirst(deck, face, suit);
+	int operation = 0; //存玩家的操作
+	fillDeckFaceFirst(deck, face, suit); //填入牌
 
-	int score[4] = {0};
+	int score[4] = {0}; //每個人的分數先歸零
 
 	system("cls");
 	while (true)
@@ -53,30 +56,31 @@ int main()
 		printf("\n動作選項:\n\t(1)重新開始(重設開始積分)\n\t(2) 繼續下一回\n\t(3) 不玩了\n");
 		printf("Input your operation(1~3):");
 		scanf_s("%d", &operation, 10);
-		if (operation == 1)
+
+		if (operation == 1) //遊戲重置
 		{
 			system("cls");
 			for (i = 0; i < 4; i++)
 			{
-				score[i] = 0;
+				score[i] = 0; //歸零每個人的分數
 			}
 			printf("遊戲重制\n");
-			gameInit();
+			gameInit(); //重新初始化
 			system("cls");
 			printf("遊戲已重制\n");
 			system("pause");
 			system("cls");
 		}
-		else if (operation == 2)
+		else if (operation == 2) //繼續遊戲
 		{
-			shuffle(deck);
+			shuffle(deck); //洗牌
 
 
 			int changeCardIndexBuffer[4][3]; //暫存要換的牌是哪些
 
 			int scoreThisRoundTemp[4][3]; //暫存分數 4個玩家 前中後3個墩
 
-			int currentPlayer;
+			int currentPlayer; //現在的動作是針對哪位玩家
 
 
 			//選要換的牌
@@ -85,14 +89,13 @@ int main()
 				system("cls");
 				printf("\t現在輪到第%d位玩家\n\t你要跟第%d位玩家換牌\n", currentPlayer + 1, changeWithWhom[currentPlayer] + 1);
 				printf("\n以下是您的牌\n");
-				//system("pause");
-				sort(handCard[currentPlayer], 13);
-				deal(handCard[currentPlayer], 13);
+				sort(handCard[currentPlayer], 13); //先整理
+				deal(handCard[currentPlayer], 13); //再印出牌
 				printf("\n請選擇三張你要換的牌:");
 				for (i = 0; i < 3; i++)
 				{
-					scanf_s("%d", &changeCardIndexBuffer[currentPlayer][i]);
-					changeCardIndexBuffer[currentPlayer][i]--;
+					scanf_s("%d", &changeCardIndexBuffer[currentPlayer][i]); //輸入要換的牌是哪幾張
+					changeCardIndexBuffer[currentPlayer][i]--; //牌的編號是從1開始,但振烈index是從0開始
 				}
 			}
 			system("cls");
@@ -103,14 +106,25 @@ int main()
 			{
 				for (i = 0; i < 3; i++)
 				{
+					//SWAP換牌
+
+					//先暫存第currentPlayer位玩家手牌的第"currentPlayer要換的牌"張牌;
+					//簡單來說就是要換出的牌
 					Card temp = handCard[currentPlayer][changeCardIndexBuffer[currentPlayer][i]];
+
+					//換牌目標:"要跟currentPlayer換牌的玩家" 的手牌的第"(要跟currentPlayer換牌的玩家)要換的牌" 張牌
+					//簡單來說就是要換入的牌
 					Card toBeChanged = handCard[changeWithWhom[currentPlayer]][changeCardIndexBuffer[changeWithWhom[
 						currentPlayer]][i]];
+
+					//兩張牌交換
 					handCard[currentPlayer][changeCardIndexBuffer[currentPlayer][i]] = toBeChanged;
 					handCard[changeWithWhom[currentPlayer]][changeCardIndexBuffer[changeWithWhom[currentPlayer]][i]] =
 						temp;
 				}
-				if (changeWithWhom[0] == 1) //如果是1 2 互換
+
+				//互換的對象不用重複換(假如規則是13互換,那互換執行在第一個玩家時第三位也換好了)
+				if (changeWithWhom[0] == 1) //如果是1 2 互換，那互換的動作只需要作用在第1位和第3位就好
 				{
 					if (currentPlayer == 0)
 					{
@@ -121,7 +135,7 @@ int main()
 						break;
 					}
 				}
-				else
+				else //如果是 13換 或是 14換，互換只要執行到第二位就全部換完了
 				{
 					if (currentPlayer == 1)
 					{
@@ -134,15 +148,15 @@ int main()
 			//排列牌組，順便計算手牌分數
 			for (currentPlayer = 0; currentPlayer < playerCount; currentPlayer++)
 			{
-				Card handCardTemp[13];
-				int indexTemp; //暫存排好的牌
+				Card handCardTemp[13]; //暫存手牌
+				int indexTemp; //暫存玩家輸入的牌的編號
 				printf("\t現在輪到第%d位玩家\n", currentPlayer + 1);
 				printf("\n以下是您的牌\n");
-				//system("pause");
 				sort(handCard[currentPlayer], 13);
 				deal(handCard[currentPlayer], 13);
-				i = 0;
-				//前墩三張
+
+				//先把玩家輸入的排序存到handCardTemp中,再複製回handCard				
+				//暫存前墩三張
 				printf("請輸入前墩三張:");
 				for (i = 0; i < 3; i++)
 				{
@@ -150,14 +164,14 @@ int main()
 					handCardTemp[i] = handCard[currentPlayer][indexTemp - 1];
 				}
 
-				//中墩五張
+				//暫存中墩五張
 				printf("請輸入中墩五張:");
 				for (; i < 8; i++)
 				{
 					scanf_s("%d", &indexTemp, 10);
 					handCardTemp[i] = handCard[currentPlayer][indexTemp - 1];
 				}
-				//後墩五張
+				//暫存後墩五張
 				printf("請輸入後墩五張:");
 				for (; i < 13; i++)
 				{
@@ -165,20 +179,21 @@ int main()
 					handCardTemp[i] = handCard[currentPlayer][indexTemp - 1];
 				}
 
+				//把handCardTemp再複製回handCard
 				for (i = 0; i < 13; i++)
 				{
 					handCard[currentPlayer][i] = handCardTemp[i];
 				}
 
-				sort(handCard[currentPlayer], 3);
-				sort(handCard[currentPlayer] + 3, 5);
-				sort(handCard[currentPlayer] + 8, 5);
+				sort(handCard[currentPlayer], 3); //排序前墩
+				sort(handCard[currentPlayer] + 3, 5); //排序中墩
+				sort(handCard[currentPlayer] + 8, 5); //排序後墩
 
 				printf("\n換牌後:\n");
 				deal(handCard[currentPlayer], 13);
 
 				//計算手牌的分數
-				if (isStraight(handCard[currentPlayer], 13) == 1) //如果是一條龍就讓她贏了
+				if (isStraight(handCard[currentPlayer], 13) == 1) //特例:如果是一條龍就讓她贏了
 				{
 					printf("你的牌是一條龍!!");
 					for (i = 0; i < 3; i++)
@@ -186,8 +201,9 @@ int main()
 						scoreThisRoundTemp[currentPlayer][i] = 100000;
 					}
 				}
-				else //存入分數
+				else //正常狀況下存入分數
 				{
+					//利用determine印出牌型,利用giveScore手牌的分數
 					printf("\n前墩: %s  分數:%d\n", cardType[determine(handCard[currentPlayer], 3)],
 					       giveScore(handCard[currentPlayer], 3));
 					printf("中墩: %s 分數:%d\n", cardType[determine(handCard[currentPlayer] + 3, 5)],
@@ -195,10 +211,14 @@ int main()
 					printf("後墩: %s 分數:%d\n", cardType[determine(handCard[currentPlayer] + 8, 5)],
 					       giveScore(handCard[currentPlayer] + 8, 5));
 
+
+					//把各玩家的三墩分數暫存起來
 					scoreThisRoundTemp[currentPlayer][0] = giveScore(handCard[currentPlayer], 3);
 					scoreThisRoundTemp[currentPlayer][1] = giveScore(handCard[currentPlayer] + 3, 5);
 					scoreThisRoundTemp[currentPlayer][2] = giveScore(handCard[currentPlayer] + 8, 5);
 
+
+					//規則判斷: 後墩必須大於等於中墩，中墩必須大於等於前墩，否則視為三墩全輸
 					if (scoreThisRoundTemp[currentPlayer][0] >= scoreThisRoundTemp[currentPlayer][1] ||
 						scoreThisRoundTemp[currentPlayer][1] >= scoreThisRoundTemp[currentPlayer][2])
 					{
@@ -206,7 +226,7 @@ int main()
 						printf("(後墩必須大於等於中墩，中墩必須大於等於前墩，否則視為三墩全輸)\n\n");
 						for (i = 0; i < 3; i++)
 						{
-							scoreThisRoundTemp[currentPlayer][i] = 0;
+							scoreThisRoundTemp[currentPlayer][i] = 0; //分數值歸零
 						}
 					}
 				}
@@ -218,6 +238,7 @@ int main()
 
 
 			system("cls");
+
 			//計算比較各墩後的最終分數
 			for (currentPlayer = 0; currentPlayer < playerCount; currentPlayer++)
 			{
@@ -226,7 +247,7 @@ int main()
 				printf("我自己\t%d\t%d\t%d\n", scoreThisRoundTemp[currentPlayer][0], scoreThisRoundTemp[currentPlayer][1],
 				       scoreThisRoundTemp[currentPlayer][2]);
 				printf("\n");
-				int homerunCount = 0;
+				int homerunCount = 0; //計算全贏的場數
 				for (i = 0; i < playerCount; i++)
 				{
 					if (i != currentPlayer)
@@ -235,13 +256,13 @@ int main()
 						       scoreThisRoundTemp[i][2]);
 					}
 				}
-				printf("\n\n\n");
+				printf("\n\n\n"); //印出和其他玩家的對照
 
 				for (i = 0; i < playerCount; i++)
 				{
-					int winTimesWithOtherPlayer = 0;
-					int tieTimesWithOtherPlayer = 0;
-					if (i != currentPlayer)
+					int winTimesWithOtherPlayer = 0; //儲存和另一個玩家比較 贏的場次
+					int tieTimesWithOtherPlayer = 0; //儲存和另一個玩家比較 平手的場次
+					if (i != currentPlayer) //不用和自己比
 					{
 						for (j = 0; j < 3; j++)
 						{
@@ -254,28 +275,29 @@ int main()
 								tieTimesWithOtherPlayer++;
 							}
 						}
-						int totalPlus = 0;
-						if (tieTimesWithOtherPlayer == 3)
+						int totalPlus = 0; //暫存總分要加的分
+
+						if (tieTimesWithOtherPlayer == 3) //全部平手 加0分
 						{
 							totalPlus = 0;
 						}
-						else if (winTimesWithOtherPlayer == 3)
+						else if (winTimesWithOtherPlayer == 3) //全贏 加6分
 						{
 							score[currentPlayer] = score[currentPlayer] + 6;
 							totalPlus = 6;
 							homerunCount++;
 						}
-						else if (winTimesWithOtherPlayer == 2)
+						else if (winTimesWithOtherPlayer == 2) //輸一場 加2分
 						{
 							score[currentPlayer] = score[currentPlayer] + 2;
 							totalPlus = 2;
 						}
-						else if (winTimesWithOtherPlayer == 1)
+						else if (winTimesWithOtherPlayer == 1) //贏一場 減1分
 						{
 							score[currentPlayer] = score[currentPlayer] - 1;
 							totalPlus = -1;
 						}
-						else if (winTimesWithOtherPlayer == 0)
+						else if (winTimesWithOtherPlayer == 0) //全輸 減6分
 						{
 							score[currentPlayer] = score[currentPlayer] - 6;
 							totalPlus = -6;
@@ -285,7 +307,7 @@ int main()
 						printf("第%d位玩家 對第%d位玩家 總分加%d\n", currentPlayer + 1, i + 1, totalPlus);
 					}
 				}
-				if (homerunCount >= 3 && playerCount == 4)
+				if (homerunCount >= 3 && playerCount == 4) //如果對其他玩家都是全贏,就HOMERUN(僅限4人局)
 				{
 					printf("恭喜你紅不讓了!!! 總分再加18分\n");
 					score[currentPlayer] = score[currentPlayer] + 18;
@@ -297,7 +319,7 @@ int main()
 			system("pause");
 			system("cls");
 		}
-		else if (operation == 3)
+		else if (operation == 3) //結束遊戲
 		{
 			break;
 		}
@@ -324,16 +346,18 @@ void gameInit()
 	}
 	if (playerCount == 4)
 	{
-		int buffer[4]; //暫存
+		int buffer[4]; //暫存輸入的數字
 		printf("制定換牌規則(輸入四個玩家代號 第一二互換 三四戶換):");
 		for (i = 0; i < 4; i++)
 		{
 			scanf_s("%d", &buffer[i], 10);
 		}
+
+		//存入要跟誰換(index是從0開始，玩家編號是從 1開始)
 		changeWithWhom[buffer[0] - 1] = buffer[1] - 1;
-		changeWithWhom[buffer[1] - 1] = buffer[0] - 1;
+		changeWithWhom[buffer[1] - 1] = buffer[0] - 1; //輸入的1,2位互換
 		changeWithWhom[buffer[2] - 1] = buffer[3] - 1;
-		changeWithWhom[buffer[3] - 1] = buffer[2] - 1;
+		changeWithWhom[buffer[3] - 1] = buffer[2] - 1; //輸入的3,4位互換
 	}
 	else
 	{
